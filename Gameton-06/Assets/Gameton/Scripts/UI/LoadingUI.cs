6e3e4e686345a6b79792e24f7fa45a78b5543338
aube.lee <leejjoo112@icloud.com>
2025-02-02 14:44:12 +0900

    LoadingUI 재구성

--- Gameton-06/Assets/Gameton/Scripts/UI/LoadingUI.cs
+++ Gameton-06/Assets/Gameton/Scripts/UI/LoadingUI.cs
@@ -6,6 +6,29 @@ namespace TON
 {
     public class LoadingUI : UIBase
     {
+        // private Animator playerAnimator;
 
+        // private void Start()
+        // {
+        //     PlayerSpawner.SpawnPlayerCharacter();
+        //     // TON.Player 하위의 Animator 찾기
+        //     GameObject playerObj = GameObject.Find("TON.Player");
+        //     if (playerObj != null)
+        //     {
+        //         playerAnimator = playerObj.GetComponentInChildren<Animator>();
+        //         if (playerAnimator != null)
+        //         {
+        //             playerAnimator.SetBool("IsMoving", true);  // Loading UI에서만 달리게 설정
+        //         }
+        //     }
+        // }
+
+        // private void OnDisable()  // Loading UI가 사라질 때
+        // {
+        //     if (playerAnimator != null)
+        //     {
+        //         playerAnimator.SetBool("IsMoving", false);  // 인게임 시작 전 멈추도록 설정
+        //     }
+        // }
     }
 }

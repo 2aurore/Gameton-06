7e81853eeda277e046507d9087d4478ec4645fb1
Mingu Kim <mcutegs2@me.com>
2025-02-03 23:59:23 +0900

    Monstercontrol 코드 수정
    데이터테이블 기반 기본 값 추가, 애니메이션 수정

--- Gameton-06/Assets/Gameton/Scripts/Monster/MonsterControl.cs
+++ Gameton-06/Assets/Gameton/Scripts/Monster/MonsterControl.cs
@@ -7,7 +7,19 @@ namespace TON
 {
     public class MonsterControl : MonoBehaviour
     {
-        public float speed = 2;
+        public int id;  // 적 고유 ID
+        public string name; // 적 이름
+        public int hp;  // HP
+        public int damage;  // 공격력
+        public int defense; // 방어력
+        public int speed = 2; // 이동속도
+        public string aiType; // AI 패턴?
+        public int dropItems;   // 드롭 아이템 ID
+        public int eXperiencePoint; // 경험치?
+        
+        
+        
+        public Animator animator;
 
         GameObject target;
         
@@ -15,6 +27,9 @@ namespace TON
         void Start()
         {
             target = GameObject.FindGameObjectWithTag("Player");
+            animator = GetComponent<Animator>();
+            
+            
         }
         
         // Update is called once per frame
@@ -30,6 +45,8 @@ namespace TON
             
             // 방향 * 속도 * 시간간격 
             transform.Translate(direction.normalized * speed * Time.deltaTime);
+            // animator.SetBool("Iidle", true);
+            animator.SetBool("Walk", true );    // 걷기 애니메이션
         }
     }
 }

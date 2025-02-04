44777fdad6fec8ddf99c96ffbfc8d6e7abce6b16
Mingu Kim <mcutegs2@me.com>
2025-02-04 19:51:09 +0900

    인게입 UI, 인게임 옵션 UI 수정

--- Gameton-06/Assets/Gameton/Scripts/Common/BootStrapper.cs
+++ Gameton-06/Assets/Gameton/Scripts/Common/BootStrapper.cs
@@ -49,8 +49,9 @@ namespace TON
 
 
             // TODO : Custom Order After System Load
-            // UIManager.Show<IngameUI>(UIList.IngameUI);
+            UIManager.Show<IngameUI>(UIList.IngameUI);
             UIManager.Show<ControllerUI>(UIList.ControllerUI);
+            UIManager.Show<IngameOptionUI>(UIList.IngameOptionUI);
             // UIManager.Show<CharaterSelectUI>(UIList.CharaterSelectUI);
 
             // UIManager.Show<TitleUI>(UIList.TitleUI);

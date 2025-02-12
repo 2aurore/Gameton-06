a8ff9955ed395c250130a06a650d29ac177f6ae0
Mingu Kim <mcutegs2@me.com>
2025-02-12 15:43:46 +0900

    몬스터 json 로드를 위한 코드 추가

--- Gameton-06/Assets/Gameton/Scripts/Monster/MonsterDataManager.cs
+++ Gameton-06/Assets/Gameton/Scripts/Monster/MonsterDataManager.cs
@@ -8,72 +8,47 @@ namespace TON
     using System.IO;
     using UnityEngine;
 
-    public class MonsterDataManager : MonoBehaviour
+    public class MonsterDataManager : SingletonBase<MonsterDataManager>
     {
-        public static MonsterDataManager Instance { get; private set; }
-        public Dictionary<int, MonsterData> monsterDataDict = new Dictionary<int, MonsterData>();
+        // 전체 몬스터 데이터 리스트
+        public List<MonsterData> monstersData { get; private set; }
 
-        // void Awake()
-        // {
-        //     if (Instance == null)
-        //     {
-        //         Instance = this;
-        //     }
-        //     else
-        //     {
-        //         Destroy(gameObject);
-        //         return;
-        //     }
-        //
-        //     LoadMonsterData("monster_data.csv"); // CSV 파일 이름
-        // }
+        // 현재 선택된 몬스터 데이터
+        public MonsterData currentMonster { get; private set; }
 
-        // void LoadMonsterData(string fileName)
-        // {
-        //     List<string[]> data = ReadCSV(fileName);
-        //
-        //     // 첫 번째 행은 헤더이므로 건너뜀
-        //     for (int i = 1; i < data.Count; i++)
-        //     {
-        //         string[] row = data[i];
-        //         MonsterData monsterData = new MonsterData();
-        //
-        //         monsterData.id = int.Parse(row[0]);
-        //         monsterData.name = row[1];
-        //         monsterData.attackPower = int.Parse(row[2]);
-        //         monsterData.health = int.Parse(row[3]);
-        //         monsterData.speed = float.Parse(row[4]);
-        //
-        //         monsterDataDict.Add(monsterData.id, monsterData);
-        //     }
-        // }
-        //
-        // List<string[]> ReadCSV(string fileName)
-        // {
-        //     List<string[]> data = new List<string[]>();
-        //     TextAsset textAsset = Resources.Load<TextAsset>(fileName); // Resources 폴더에서 파일 로드
-        //
-        //     using (StringReader reader = new StringReader(textAsset.text))
-        //     {
-        //         while (!reader.EndOfStream)
-        //         {
-        //             string line = reader.ReadLine();
-        //             string[] row = line.Split(',');
-        //             data.Add(row);
-        //         }
-        //     }
-        //
-        //     return data;
-        // }
+        protected override void Awake()
+        {
+            base.Awake();
+            LoadMonsterData();
+        }
+
+        private void LoadMonsterData()
+        {
+            monstersData = JSONLoader.LoadFromResources<List<MonsterData>>("Monster");
+            if (monstersData == null)
+            {
+                monstersData = new List<MonsterData>();
+                Debug.LogError("몬스터 데이터 로드 실패");
+            }
+        }
+
+        public MonsterData GetMonsterData(int monsterId)
+        {
+            if (monsterId >= 0 && monsterId < monstersData.Count)
+            {
+                currentMonster = monstersData[monsterId];
+                return currentMonster;
+            }
+            else
+            {
+                Debug.LogError($"유효하지 않은 몬스터 ID입니다: {monsterId}");
+                return null;
+            }
+        }
+
+        public List<MonsterData> GetAllMonsterData()
+        {
+            return monstersData;
+        }
     }
-}
-    
-// 몬스터 데이터 클래스
-public class MonsterData
-{
-    public int id;
-    public string name;
-    public int attackPower;
-    public int health;
-    public float speed;
 }

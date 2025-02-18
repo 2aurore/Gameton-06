888a308e1938360d3e157562200eccbaba8ad090
aube.lee <leejjoo112@icloud.com>
2025-02-18 15:06:08 +0900

    feat: 스테이지 재입장 버튼 클릭 액션 추가

--- Gameton-06/Assets/Gameton/Scripts/UI/PauseUI.cs
+++ Gameton-06/Assets/Gameton/Scripts/UI/PauseUI.cs
@@ -36,6 +36,20 @@ namespace TON
             // TODO: 스테이지 재시작 로직 추가
             Time.timeScale = 1f;
             Debug.Log("OnClickStageRetryButton");
+
+            // 가지고 있는 하트가 없다면 입장 불가
+            if (HeartDataManager.Singleton.GetCurrentHearts() < 1)
+            {
+                Time.timeScale = 0f;
+                // TODO: 입장 불가 modal 출력
+                Debug.Log("보유한 하트 없음");
+                return;
+            }
+
+            // 입장 시 하트 소모
+            HeartDataManager.Singleton.UseHeart();
+
+            Main.Singleton.ChangeScene(SceneType.Stage);
         }
     }
 }

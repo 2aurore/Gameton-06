6870d2c29d627729d2eacf38c7509650dc2614dd
aube.lee <leejjoo112@icloud.com>
2025-02-26 11:23:54 +0900

    fix: using UnityEditor 수정

--- Gameton-06/Assets/Gameton/Scripts/Common/SingletonBase.cs
+++ Gameton-06/Assets/Gameton/Scripts/Common/SingletonBase.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 
 namespace TON
 {

6c75822d6aeb6142d68a30d7bb3deda9d667a3bf
Mingu Kim <mcutegs2@me.com>
2025-02-27 18:16:03 +0900

    몬스터 데이터 json 항목에 맞춰 수정

--- Gameton-06/Assets/Gameton/Scripts/GameData/MonsterData.cs
+++ Gameton-06/Assets/Gameton/Scripts/GameData/MonsterData.cs
@@ -13,27 +13,19 @@ namespace TON
         public string name;
         // 몬스터 레벨
         public int level;
-        // 몬스터 타입 ex : melee, ranged
-        public string monsterType;
         // 몬스터 체력
         public int hp;
         // 기본 공격력
         public int attackPower;
         // 기본 방어력
         public int defencePower;
-        // 몬스터 스킬 ID 1 
+        // 몬스터 스킬 ID 
         public int monsterSkillID;
-        // 몬스터 스킬 ID 2
-        public int monsterSkillIDTwo;
-        // 패트롤 범위
-        public float patrolRange;
-        // 인식 범위
-        public float detectionRange;
-        // 추적 범위
-        public float chaseRange;
-        public float moveSpeed;
-        // 공격 범위
-        public float attackRange;
-
+        // 골드
+        public int Gold;
+        // 경험치
+        public int Exp;
+        // 점수
+        public int Score;
     }
 }

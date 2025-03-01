1c31056e98712f1a12c0a6d07930ee56a8246d28
Mingu Kim <mcutegs2@me.com>
2025-03-01 21:03:13 +0900

    씬 전환될때 이전 씬 노래 꺼지지 않는 문제 수정

--- Gameton-06/Assets/Gameton/Scripts/Sound/SoundManager.cs
+++ Gameton-06/Assets/Gameton/Scripts/Sound/SoundManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace TON
 {
@@ -21,6 +22,24 @@ namespace TON
             {
                 Destroy(gameObject);
             }
+        }
+        
+        private void OnEnable()
+        {
+            // 씬 로드 이벤트에 리스너 등록
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+        
+        private void OnDisable()
+        {
+            // 씬 로드 이벤트에서 리스너 제거
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+        
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            // 씬이 바뀔 때마다 배경 음악 종료
+            BgSoundPlay(null);
         }
 
         public void SFXPlay(string sfxName, AudioClip clip)

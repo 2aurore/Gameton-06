c6b9154ba5d33b58c1280da17e50d6daddebd17b
aube.lee <leejjoo112@icloud.com>
2025-03-12 00:29:24 +0900

    docs: 프로젝트 변경으로 사용하지 않는 파일 모두 정리

--- Gameton-06/Assets/Gameton/Scripts/Scenes/StageScene.cs
+++ Gameton-06/Assets/Gameton/Scripts/Scenes/StageScene.cs
@@ -12,29 +12,8 @@ namespace TON
 
         public override IEnumerator OnStart()
         {
-            string stageId = PlayerPrefs.GetString("StageId", null);
-            Assert.IsNotNull(stageId, "StageScene:: 스테이지 진입 실패!");
-
             // 선택한 스테이지에 맞는 씬을 로드한다
-            AsyncOperation asyncLoad = null;
-            switch (stageId)
-            {
-                case "STG001":
-                    asyncLoad = SceneManager.LoadSceneAsync("Stage 1", LoadSceneMode.Single);
-                    break;
-                case "STG002":
-                    asyncLoad = SceneManager.LoadSceneAsync("Stage 2", LoadSceneMode.Single);
-                    break;
-                case "STG003":
-                    asyncLoad = SceneManager.LoadSceneAsync("Stage 3", LoadSceneMode.Single);
-                    break;
-                case "STG004":
-                    asyncLoad = SceneManager.LoadSceneAsync("Stage 4", LoadSceneMode.Single);
-                    break;
-                default:
-                    Assert.IsTrue(false, "StageScene:: 스테이지 진입 실패!");
-                    break;
-            }
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Stage 4", LoadSceneMode.Single);
 
             // 로드가 완료될 때 까지 yield return null 을 하면서 기다린다
             while (asyncLoad != null && !asyncLoad.isDone)
@@ -48,7 +27,7 @@ namespace TON
                 Debug.LogError("Failed to spawn player character!");
                 // 에러 UI를 표시하거나 씬을 다시 로드하는 등의 처리
                 // UIManager.Show<ErrorUI>(UIList.ErrorUI);
-                // 또는 SceneManager.LoadScene("ErrorScene"); 등                
+                // 또는 SceneManager.LoadScene("ErrorScene"); 등
                 yield break;
             }
 
